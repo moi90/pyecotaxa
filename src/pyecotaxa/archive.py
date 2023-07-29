9a064675d3d17f32baaaeaba74054eb0c8668596
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2023-07-29 20:33:11 +0200

    DirectoryArchive

--- src/pyecotaxa/archive.py
+++ src/pyecotaxa/archive.py
@@ -4,6 +4,7 @@ import collections
 import csv
 import fnmatch
 import io
+import os
 import pathlib
 import posixpath
 import shutil
@@ -534,7 +535,7 @@ class TarArchive(Archive):
 
     @staticmethod
     def is_readable(archive_fn):
-        return tarfile.is_tarfile(archive_fn)
+        return os.path.isfile(archive_fn) and tarfile.is_tarfile(archive_fn)
 
     def __init__(self, archive_fn: Union[str, pathlib.Path], mode: str = "r"):
         self._tar = tarfile.open(archive_fn, mode)
@@ -604,7 +605,7 @@ class ZipArchive(Archive):
 
     @staticmethod
     def is_readable(archive_fn):
-        return zipfile.is_zipfile(archive_fn)
+        return os.path.isfile(archive_fn) and zipfile.is_zipfile(archive_fn)
 
     def __init__(self, archive_fn: Union[str, pathlib.Path], mode: str = "r"):
         self._zip = zipfile.ZipFile(archive_fn, mode)
@@ -644,3 +645,41 @@ class ZipArchive(Archive):
 
     def close(self):
         self._zip.close()
+
+
+class DirectoryArchive(Archive):
+    extensions = [""]
+
+    @staticmethod
+    def is_readable(archive_fn):
+        return os.path.isdir(archive_fn)
+
+    def __init__(self, archive_fn: Union[str, pathlib.Path], mode: str = "r"):
+        self.archive_fn = archive_fn
+        self.mode = mode
+
+    def members(self):
+        def findall():
+            for root, dirs, files in os.walk(self.archive_fn):
+                relroot = os.path.relpath(root, self.archive_fn)
+                for fn in files:
+                    yield os.path.join(relroot, fn)
+
+        return list(findall())
+
+    def open(self, member_fn: str, mode="r", compress_hint=True) -> IO:
+        del compress_hint
+        return open(os.path.join(self.archive_fn, member_fn), mode=mode)
+
+    def write_member(
+        self, member_fn: str, fileobj_or_bytes: Union[IO, bytes], compress_hint=True
+    ):
+        del compress_hint
+        with self.open(member_fn, "w") as f:
+            if isinstance(fileobj_or_bytes, bytes):
+                f.write(fileobj_or_bytes)
+            else:
+                shutil.copyfileobj(fileobj_or_bytes, f)
+
+    def close(self):
+        pass
