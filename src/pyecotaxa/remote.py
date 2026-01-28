29b8972d3aa878a5ddf3fb5c8e5773f25c777e47
Simon-Martin Schröder <mschroeder@geomar.de>
2026-01-28 09:14:53 +0000

    Remote: Add configurable retry parameter for HTTP requests

--- src/pyecotaxa/remote.py
+++ src/pyecotaxa/remote.py
@@ -15,7 +15,7 @@ import urllib.parse
 import uuid
 import warnings
 import zipfile
-from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, List, Literal, Mapping, Optional, Sequence, Tuple, Union
 
 import requests
 import requests.adapters
@@ -213,6 +213,7 @@ class Remote:
         exported_data_share: Union[None, str, bool] = None,
         import_data_share: Union[None, str, bool] = None,
         verbose=False,
+        retry: Union[int, Literal[False]] = 3,
     ):
         super().__init__()
 
@@ -248,10 +249,11 @@ class Remote:
 
         # TODO: Use session everywhere
         self._session = requests.Session()
-        retry = urllib3.util.retry.Retry(connect=3, backoff_factor=0.5)
-        adapter = requests.adapters.HTTPAdapter(max_retries=retry)
-        self._session.mount("http://", adapter)
-        self._session.mount("https://", adapter)
+        if retry:
+            retry_cfg = urllib3.util.retry.Retry(retry, backoff_factor=0.5)
+            adapter = requests.adapters.HTTPAdapter(max_retries=retry_cfg)
+            self._session.mount("http://", adapter)
+            self._session.mount("https://", adapter)
 
         self._check_version()
 
